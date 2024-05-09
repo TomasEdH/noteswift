80a64b3c3f8b2002ef92d99c05d190796431e4d1
TomasEdH <tomasestrada1209@gmail.com>
2024-05-09 13:36:44 -0500

    refactor: Change origin cors client

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ mongoose.connect(MONGO_URI);
 app.use(
   cors({
     origin: "http://localhost:3000",
-    origin: "https://noteswift-api.vercel.app",
+    origin: "https://noteswift-client.vercel.app",
     credentials: true,
   })
 );
